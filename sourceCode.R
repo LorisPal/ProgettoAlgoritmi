dbef0f006bea33c00997d0fa488305ba1b25a87c
loris <generalle913@gmail.com>
2018-07-07 20:14:59 +0200

    adding a matrix function

--- sourceCode.R
+++ sourceCode.R
@@ -1,28 +1,54 @@
 graficoRc <-
   setRefClass(
     "graficoRc",
-    fields = list(nodo = "vector", arco = "matrix", trovato="logical"),
+    fields = list(
+      nodo = "vector",
+      arco = "matrix",
+      trovato = "logical"
+    ),
     methods = list(
-      initialize = function() {
-        nodo <<- 0
-        arco <<- matrix(0,1,1)
-      },
-      
       searchNode = function(node) {
         trovato <<- FALSE
         for (i in 1:length(nodo)) {
-            if(node == nodo[[i]]) {
-              trovato <<- TRUE
-              break;
-            }
+          if (node == nodo[[i]]) {
+            trovato <<- TRUE
+            break
+            
           }
-        if(trovato) {print("Nodo trovato: non aggiunto");}
-        else {print("Nodo non trovato");}
+        }
+        if (trovato) {
+          print("Nodo trovato: nodo non aggiunto")
+        }
+        else {
+          print("Nodo non trovato: nodo aggiunto")
+        }
       },
       
       addNode = function(x) {
-        searchNode(x)
-        if(!trovato) {nodo <<- c(nodo, x);}
+        if (length(nodo) < 1) {
+          nodo <<- c(nodo, x)
+          
+        }
+        else{
+          searchNode(x)
+          if (!trovato) {
+            nodo <<- c(nodo, x)
+          }
+        }
+      },
+      
+      addArco = function(x, nodoP, nodoA){
+        arco <<- matrix(0, length(nodo), length(nodo))
+        rownames(arco) <<- nodo
+        colnames(arco) <<- nodo
+        for (j in 1:length(nodo)) {
+          for (i in 1:length(nodo)) {
+            if(nodoP==nodo[[i]] && nodoA==nodo[[j]]){
+            arco[[i,j]] <<- x
+            }
+          }
+        }
+        print(arco)
       }
     )
   )
